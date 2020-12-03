5a4a98b7399b176a0c0e5b7edeea71c4e4826974
Nkwe Justice Mapoulo <nkwejustice1@gmail.com>
2020-12-03 11:30:11 +0200

    first commit

---
+++ AutorizationServer/src/test/java/com/AutorizationServer/demo/AutorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutorizationServer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
